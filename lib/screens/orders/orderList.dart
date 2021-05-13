897338621f67fe2a966b897295ee3304c0a7c8a8
blayziken <kingtoluwalope@gmail.com>
2021-05-13 21:12:49 +0100

    added food order list view

---
+++ lib/screens/orders/orderList.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class OrderList extends StatelessWidget {
+  static const routeName = '/order-list';
+
+  OrderList({this.order});
+
+  final List order;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget fullList() {
+      List<Widget> jjj = [];
+      for (var i = 0; i < order.length; i++) {
+        for (var j = 0; j < order[i].length; j++) {
+//          print('${order[i][j]}');
+
+          jjj.add(
+            Container(
+                width: 250,
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('${order[i][j]}', style: TextStyle(fontSize: 20)),
+                        Text('${order[i][++j]}',
+                            style: TextStyle(fontSize: 20)),
+                      ],
+                    ),
+                    SizedBox(height: 50),
+                  ],
+                )),
+          );
+        }
+      }
+      print(order);
+      print(jjj);
+      return Column(
+//          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: jjj);
+    }
+
+    var media = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.lightBlue,
+        title: Text('Order List'),
+      ),
+      body: SafeArea(
+        child: Container(
+          width: media.width,
+          height: media.height,
+//          color: Colors.red,
+          child: Padding(
+            padding: EdgeInsets.only(top: 50.0),
+            child: Column(
+              children: [
+                Expanded(
+                  child: Container(
+                    height: 400,
+                    width: 350,
+                    child: fullList(),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
